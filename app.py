4e9f622fadef2773ea919ed2b27a7adcb174423f
easycode002 <easycode002@gmail.com>
2025-02-07 10:54:14 +0700

    Added: Working for config api document via fast swagger ui.

--- app.py
+++ app.py
@@ -1,26 +1,13 @@
-from app import create_app,db
-from flask_migrate import Migrate
+from typing import Union
+
 from fastapi import FastAPI
-from flask_fastapi import FastAPIApp
 
-# Intialize Flask App
-flask_app = create_app()
-migrate = Migrate(flask_app,db)
+app = FastAPI()
 
-# Intialize FastAPI
-fastapi_app = FastAPI(
-    title="Flask Shop",
-    version="0.0.1",
-    description="None",
-    swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"}
-)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-@fastapi_app.get('/fastapi')
-async def fastapi_route():
-    return {'message':'Hello from FalstAPI'}
-
-# Mount FastAPI inside Flask
-flask_app.wsgi_app=FastAPIApp(fastapi_app,flask_app.wsgi_app,prefix="/api")
-
-if __name__ == "__main__":
-    flask_app.run(host='0.0.0.0',port=5000,debug=True)
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
